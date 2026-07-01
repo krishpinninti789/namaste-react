be084838b4f42194aefa42342b35deb10166abb4
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-01 20:53:53 +0530

    Refactor App.js to use functional components and update index.html to include initial content

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "This is h1"),
-  ),
-  React.createElement("h2", {}, "This is h2"),
-]);
+
+const heading = <h1>Hello bull reddy</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+const Title = () => <h1>This is title from the component composition</h1>;
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1>Hello from the functional component</h1>
+    </div>
+  );
+};
+
+root.render(<HeadingComponent />);
