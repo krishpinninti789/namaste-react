b8d3e4bc2cb7004f4762e6c44bfdc7109baf7b5c
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-12 21:22:19 +0530

    Refactor App.js to implement Header and Restaurant components, and enhance index.css for styling

--- App.js
+++ App.js
@@ -1,19 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1>Hello bull reddy</h1>;
+// Header Components
+const Logo = () => (
+  <img
+    className="logo"
+    src="https://cdn-icons-png.flaticon.com/512/3075/3075977.png"
+    alt="logo"
+    width="70"
+  />
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const Title = () => <h1>This is title from the component composition</h1>;
-
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div>
-      <Title />
-      <h1>Hello from the functional component</h1>
+    <div className="header">
+      <Logo />
+
+      <ul className="nav-items">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
     </div>
   );
 };
 
-root.render(<HeadingComponent />);
+// Restaurant Card
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=500"
+        alt="Restaurant"
+      />
+
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian</h4>
+      <h4>4.5 ⭐</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+
+// Restaurant Container
+const RestaurantContainer = () => {
+  return (
+    <div className="res-container">
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+    </div>
+  );
+};
+
+// Body
+const Body = () => {
+  return (
+    <div className="body">
+      <RestaurantContainer />
+    </div>
+  );
+};
+
+// App Layout
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
