32ec8b1d9f292227709fa949c8c7840383279d08
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-12 23:48:20 +0530

    Add filter button to display top-rated restaurants and update styles

--- src/components/RestaurantContainer.jsx
+++ src/components/RestaurantContainer.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { resList } from "../utils/resConfig";
 import RestaurantCard from "./RestaurantCard";
 
 // Restaurant Container
 const RestaurantContainer = () => {
+  const [filteredRes, setFilteredRes] = useState(resList);
+  const handleFilterTopRated = () => {
+    setFilteredRes(
+      filteredRes.filter((resItem) => Number(resItem.avgRating) > 4.4),
+    );
+  };
+  console.log(filteredRes);
   return (
     <div className="res-container">
-      {resList.map((resCardData) => (
+      <button className="filter-btn" onClick={handleFilterTopRated}>
+        Top rated Restaurants
+      </button>
+      {filteredRes.map((resCardData) => (
         <RestaurantCard resData={resCardData} key={resCardData.id} />
       ))}
     </div>
