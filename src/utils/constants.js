1897b8bad8be8ca07715982ef89851a9249b197f
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-13 18:50:05 +0530

    Update SWIGGY_API_URL to use CORS proxy for API requests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const SWIGGY_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
