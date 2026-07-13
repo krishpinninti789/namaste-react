c3f12a96fe240d612b32789dd9329cf733421aff
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-13 15:43:00 +0530

    Add login button to Header component with toggle functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Logo from "./Logo";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("login");
   return (
     <div className="header">
       <Logo />
@@ -10,6 +12,14 @@ const Header = () => {
         <li>About</li>
         <li>Contact</li>
         <li>Cart</li>
+        <button
+          className="fltr-btn"
+          onClick={() =>
+            btnName === "login" ? setBtnName("logout") : setBtnName("login")
+          }
+        >
+          {btnName}
+        </button>
       </ul>
     </div>
   );
