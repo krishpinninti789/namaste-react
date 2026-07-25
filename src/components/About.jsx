dd2152ca8917d4fa57f1bbeb4989cc9fe3c201c7
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-25 08:49:29 +0530

    Refactor: Convert About component to class-based and add lifecycle methods

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      This is about page with Users
-      <User name="Krishnavamsi" />
-      <UserClass name="vamsi" />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        This is about page with Users
+        <User name="Krishnavamsi" />
+        <UserClass name="vamsi" />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,15 +6,30 @@ class UserClass extends React.Component {
     this.state = {
       count: 0,
     };
+    console.log("Child Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Child Component Did Mount");
   }
 
   render() {
+    console.log("Child Render");
     const { name } = this.props;
     const { count } = this.state;
     return (
       <div>
         <h1>{name}</h1>
         <h2>Count = {count}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Increment Count
+        </button>
         <h2>2+ years experience</h2>
         <h2>Node</h2>
       </div>
