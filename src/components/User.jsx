8ac95186293367f56eba467f05768e16414d31bf
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-25 08:18:39 +0530

    Fix: Add count state to User and UserClass components for displaying user count

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const User = ({ name }) => {
+  const [count] = useState(0);
   return (
     <div>
       <h1>{name}</h1>
+      <h2>Count = {count}</h2>
       <h2>2+ years experience</h2>
       <h2>Node,Next,React</h2>
     </div>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,12 +3,18 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
+
   render() {
     const { name } = this.props;
+    const { count } = this.state;
     return (
       <div>
         <h1>{name}</h1>
+        <h2>Count = {count}</h2>
         <h2>2+ years experience</h2>
         <h2>Node</h2>
       </div>
