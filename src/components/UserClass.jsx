85d622fba8a07d9c722d6c74a9bd41be4b732ba1
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-07-25 07:51:07 +0530

    Fix: Initialize state in UserClass constructor and log name

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { name } = this.props;
     return (
       <div>
-        <h1>{this.props.name}</h1>
+        <h1>{name}</h1>
         <h2>2+ years experience</h2>
         <h2>Node</h2>
       </div>
