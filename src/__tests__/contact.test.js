d1c0d41417dd8f81bab49b4dc461039c97cbf8c5
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-08-30 22:38:19 +0530

    we can group the test cases and use (it) as alias to test

--- src/__tests__/contact.test.js
+++ src/__tests__/contact.test.js
@@ -2,16 +2,18 @@ import { render, screen } from "@testing-library/react";
 import ContactUs from "../components/ContactUs";
 import "@testing-library/jest-dom";
 
-test("Contact us component loaded", () => {
-  render(<ContactUs />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
+describe("Test cases of the contact us component", () => {
+  test("Contact us component loaded", () => {
+    render(<ContactUs />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should be have two input boxes", () => {
-  render(<ContactUs />);
+  it("Should be have two input boxes", () => {
+    render(<ContactUs />);
 
-  const inputBoxes = screen.getAllByRole("textbox");
+    const inputBoxes = screen.getAllByRole("textbox");
 
-  expect(inputBoxes.length).toBe(2);
+    expect(inputBoxes.length).toBe(2);
+  });
 });
