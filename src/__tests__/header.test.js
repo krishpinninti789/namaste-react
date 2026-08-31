0c4b2393d940ac68d2aeb10061b03a8ec25a6d18
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-08-31 23:21:48 +0530

    added test cases for the button with the fireEvent object

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../components/Header";
 import { Provider } from "react-redux";
 import appStore from "../utils/appStore";
@@ -13,4 +13,13 @@ it("Should render header component", () => {
       </Provider>
     </BrowserRouter>,
   );
+
+  const loginButton = screen.getByRole("button", { name: "login" });
+  expect(loginButton).toBeInTheDocument();
+
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByRole("button", { name: "logout" });
+
+  expect(logoutButton).toBeInTheDocument();
 });
