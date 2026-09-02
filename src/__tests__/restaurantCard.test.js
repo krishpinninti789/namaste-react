862c9dc24b458d1bee01ae0933b7a71350b0df2d
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-09-02 23:38:48 +0530

    added test case for the shimmer with the data id

--- src/__tests__/restaurantCard.test.js
+++ src/__tests__/restaurantCard.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import RestaurantCard from "../components/RestaurantCard";
 import MOCK_DATA from "../mocks/restaurantMock.json";
 import "@testing-library/jest-dom";
+import Shimmer from "../components/Shimmer";
 
 test("Should render restaurant card with props", () => {
   render(<RestaurantCard resData={MOCK_DATA} />);
@@ -9,4 +10,12 @@ test("Should render restaurant card with props", () => {
   const restaurantName = screen.getByText("McDonald's");
 
   expect(restaurantName).toBeInTheDocument();
+});
+
+test("Should shimmers are loading", () => {
+  render(<Shimmer />);
+
+  const shimmerDataId = screen.getByTestId("shimmer");
+
+  expect(shimmerDataId).toBeInTheDocument();
 });
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,6 +1,9 @@
 const Shimmer = () => {
   return (
-    <div className="mx-auto grid max-w-7xl gap-6 px-4 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 lg:px-8 xl:grid-cols-4">
+    <div
+      className="mx-auto grid max-w-7xl gap-6 px-4 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 lg:px-8 xl:grid-cols-4"
+      data-testid="shimmer"
+    >
       {Array.from({ length: 12 }).map((_, index) => (
         <div
           className="overflow-hidden rounded-3xl border border-slate-200 bg-white shadow-sm"
