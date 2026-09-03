1791c8854c723b4273e10410fb7aa7e8f652af60
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-09-03 23:00:21 +0530

    Testing the search text

--- src/__tests__/search.test.js
+++ src/__tests__/search.test.js
@@ -1,7 +1,8 @@
-import { act, render } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import Body from "../components/Body";
 import MOCK_DATA from "../mocks/resDataResponseMock.json";
 import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
 global.fetch = jest.fn(() => {
   return Promise.resolve({
@@ -19,4 +20,7 @@ it("Should render the search component in Body", async () => {
       </BrowserRouter>,
     );
   });
+
+  const searchText = screen.getByText("Search");
+  expect(searchText).toBeInTheDocument();
 });
